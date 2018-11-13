4c8d32bc4f5b30f3246cab41676073904f37960b
bywind <email@example.com>
2018-11-13 09:55:22 +0800

    mybatis 支持

--- boot/src/main/java/cn/bywind/boot/BootApplication.java
+++ boot/src/main/java/cn/bywind/boot/BootApplication.java
@@ -1,9 +1,13 @@
 package cn.bywind.boot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("cn.bywind.boot.dao")
+@EnableTransactionManagement
 public class BootApplication {
 
 	public static void main(String[] args) {
--- boot/src/test/java/cn/bywind/boot/BootApplicationTests.java
+++ boot/src/test/java/cn/bywind/boot/BootApplicationTests.java
@@ -1,16 +1,32 @@
 package cn.bywind.boot;
 
+import cn.bywind.boot.model.PersonDo;
+import cn.bywind.boot.service.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BootApplicationTests {
 
+	@Autowired
+	PersonService personService;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetPerson(){
+		HashMap<String,Object> params = new HashMap<String, Object>();
+		params.put("id",1);
+		PersonDo person = personService.getPerson(params);
+		System.out.println(person);
 	}
 
 }
