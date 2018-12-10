9d9237ed881d60f60f5baf05e8e2e3d9272a9bd2
bywind <email@example.com>
2018-12-10 12:39:48 +0800

    1

--- boot-mybatisplus/src/main/java/cn/bywind/boot/Generator.java
+++ boot-mybatisplus/src/main/java/cn/bywind/boot/Generator.java
@@ -13,7 +13,7 @@ public class Generator {
 
     public static void main(String[] args) {
         // 包名
-        String packageName = "cn.bywind.boot";
+        String packageName = "cn.bywind.boot2";
         boolean serviceNameStartWithI = false;//auth -> UserService, 设置成true: auth -> IUserService
         //把需要自动生成的表 放在这里!!
         generateByTables(serviceNameStartWithI, packageName, "bywind", "boot", "person");
