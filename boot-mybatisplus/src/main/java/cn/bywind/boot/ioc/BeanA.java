fd0177c6dd36bf88ed4c90545ab5597f82202c7e
bywind <email@example.com>
2019-08-12 14:01:48 +0800

    first commit

--- boot-mybatisplus/src/main/java/cn/bywind/boot/ioc/BeanA.java
+++
@@ -1,8 +0,0 @@
-package cn.bywind.boot.ioc;
-
-public class BeanA {
-
-    public void sayName(){
-        System.out.println("this is from beanA:"+this);
-    }
-}
--- boot-mybatisplus/src/main/java/cn/bywind/boot/mq/DirectSender.java
+++
@@ -1,4 +0,0 @@
-package cn.bywind.boot.mq;
-
-public class DirectSender {
-}
